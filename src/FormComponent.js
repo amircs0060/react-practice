b8b56a093bd840fd4d1a4bfe5f819e7a3ddb9f68
amircs0060 <80102762+amircs0060@users.noreply.github.com>
2023-02-23 17:44:18 +0530

    Form submit through use state

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function FormComponent() {
+  const [form, setFormData] = useState({
+    fullname: '',
+    address: '',
+    email: '',
+  });
+
+  const handleFormChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData((values) => ({ ...values, [name]: value }));
+  };
+
   function handleSubmit() {
-    console.log('from submit action');
+    console.log('from submit action', form);
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="fullname" placeholder="Name"></input>
-      <input type="text" name="address" placeholder="Address"></input>
+      <input
+        type="text"
+        name="fullname"
+        placeholder="Name"
+        value={form.fullname}
+        onChange={handleFormChange}
+      ></input>
+      <input
+        type="text"
+        name="address"
+        placeholder="Address"
+        value={form.address}
+        onChange={handleFormChange}
+      ></input>
+      <input
+        type="text"
+        name="email"
+        placeholder="Email"
+        value={form.email}
+        onChange={handleFormChange}
+      ></input>
       <input type="submit" name="submit" value="Submit"></input>
     </form>
   );
